c480e3485d4c4857f73426b9aea74c59de1d7b85
Cjgfree123 <cjg_free@163.com>
2019-02-20 11:47:31 +0800

    demo2: 饼图+全局样式配置

--- README.md
+++ README.md
@@ -1,68 +1,15 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+## 运行
 
-## Available Scripts
+npm start
 
-In the project directory, you can run:
+## 知识点
 
-### `npm start`
+1.柱状图
 
-Runs the app in the development mode.<br>
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+2.饼图(南丁格尔图、阴影)
 
-The page will reload if you make edits.<br>
-You will also see any lint errors in the console.
+3.全局配置(背景色、文本色)；每个系列的文本色(hover)、引导线颜色(hover)
 
-### `npm test`
+* 设置扇形颜色
 
-Launches the test runner in the interactive watch mode.<br>
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.<br>
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.<br>
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (Webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/code-splitting
-
-### Analyzing the Bundle Size
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size
-
-### Making a Progressive Web App
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app
-
-### Advanced Configuration
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/advanced-configuration
-
-### Deployment
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/deployment
-
-### `npm run build` fails to minify
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+* 将数值大小映射到敏感度 visualMap
--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 import React, { Component } from 'react';
 // 引入 ECharts 主模块
-import echarts from 'echarts/lib/echarts';
-// 引入柱状图
-import 'echarts/lib/chart/bar';
-// 引入提示框和标题组件
-import 'echarts/lib/component/tooltip';
-import 'echarts/lib/component/title';
-import './App.css';
+import * as echarts from 'echarts';// 引入柱状图
 
 class EchartsTest extends Component {
   componentDidMount() {
@@ -14,17 +8,99 @@ class EchartsTest extends Component {
     var myChart = echarts.init(document.getElementById('main'));
     // 绘制图表
     myChart.setOption({
-      title: { text: 'ECharts 入门示例' },
-      tooltip: {},
-      xAxis: {
-        data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+      //设置全局背景色
+      backgroundColor: '#fff',
+      // 设置全局文本样式
+      textStyle: {
+        color: '#000'
       },
-      yAxis: {},
-      series: [{
-        name: '销量',
-        type: 'bar',
-        data: [5, 20, 36, 10, 10, 20]
-      }]
+      // 将数值大小映射到敏感度
+      visualMap: {
+        // 不显示 visualMap 组件，只用于明暗度的映射
+        show: false,
+        // 映射的最小值为 80
+        min: 80,
+        // 映射的最大值为 600
+        max: 600,
+        inRange: {
+          // 明暗度的范围是 0 到 1
+          colorLightness: [0, 1]
+        }
+      },
+      series: [
+        {
+          name: '访问来源',
+          type: 'pie',
+          radius: '55%',
+          roseType: 'angle',
+          // 数据项设置方式二: {name:value}
+          data: [
+            {
+              value: 235,
+              name: '视频广告',
+              // 单个扇形的颜色
+              itemStyle: {
+                color: 'green'
+              },
+              // 每个系列的文本颜色
+              label: {
+                textStyle: {
+                  color: 'orange'
+                },
+                // 鼠标移入文本，引导线变蓝
+                emphasis: {
+                  shadowBlur: 0,
+                  shadowColor: 'blue'
+                }
+              },
+              // 引导线颜色
+              labelLine: {
+                lineStyle: {
+                  color: '#red',
+                },
+                // 鼠标移入引导线，引导线变蓝
+                emphasis: {
+                  shadowBlur: 0,
+                  shadowColor: 'blue'
+                }
+              }
+            },
+            { value: 274, name: '联盟广告' },
+            { value: 310, name: '邮件营销' },
+            { value: 335, name: '直接访问' },
+            { value: 400, name: '搜索引擎' }
+          ],
+
+          // 数据项设置方式一: 仅数值
+          // data:[12, 34, 56, 10, 23]
+
+          // 设置阴影
+          // itemStyle: {
+          //   // 阴影的大小
+          //   shadowBlur: 200,
+          //   // 阴影水平方向上的偏移
+          //   shadowOffsetX: 0,
+          //   // 阴影垂直方向上的偏移
+          //   shadowOffsetY: 0,
+          //   // 阴影颜色
+          //   shadowColor: 'rgba(0, 0, 0, 0.5)',
+          //   // hover时阴影
+          //   // emphasis: {
+          //   //   shadowBlur: 200,
+          //   //   shadowColor: 'red'
+          //   // }
+          // }
+
+          // 设置扇形颜色
+          itemStyle: {
+            // 设置扇形的颜色
+            color: '#c23531',
+            shadowBlur: 200,
+            shadowColor: 'rgba(0, 0, 0, 0.5)'
+          }
+
+        }
+      ]
     });
   }
   render() {
